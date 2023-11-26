c925a2fc8ebcb8a3a54215db067620ea3be3467c
Matthew Kehrt <mkehrt@roseandsigil.net>
2023-11-25 22:34:11 -0500

    use addEventListener instead

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { examples } from "./examples"
 
 export function main() {
-    let example_div = document.getElementById("example");
+    let example_div = document.getElementById("example")
     if (example_div != null) {
-        let example = select_example();
-        let example_html = convert_example_to_html(example);
-        example_div.innerHTML = example_html;
+        let example = select_example()
+        let example_html = convert_example_to_html(example)
+        example_div.innerHTML = example_html
     }
 }
 
 function convert_example_to_html(example: (string | [string, string])): string {
     if (typeof example == "string") {
-        return example;
+        return example
     } else {
-        let first = example[0];
-        let second = example[1];
-        let html = first + "<span id=\"exampleand\"> and </span>" + second;
+        let first = example[0]
+        let second = example[1]
+        let html = first + "<span id=\"exampleand\"> and </span>" + second
         return html
     }
 }
@@ -23,5 +23,8 @@ function convert_example_to_html(example: (string | [string, string])): string {
 function select_example(): (string | [string, string]) {
     console.log(examples)
     let example: string | [string, string] = examples[0]
-    return example;
+    return example
 }
+
+document.addEventListener('onLoad', main)
+

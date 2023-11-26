5273335cff649c21412d684e5f9904f7aa861e3c
Matthew Kehrt <mkehrt@roseandsigil.net>
2023-11-25 22:00:44 -0500

    module stuff

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import { examples } from "examples"
+import { examples } from "./examples"
 
 function main() {
     let example_div = document.getElementById("example");
     if (example_div != null) {
-        example_div.textContent = "EXAMPLE";
+        let example = select_example();
+        let example_html = convert_example_to_html(example);
+        example_div.innerHTML = example_html;
+    }
+}
+
+function convert_example_to_html(example: (string | [string, string])): string {
+    if (typeof example == "string") {
+        return example;
+    } else {
+        let first = example[0];
+        let second = example[1];
+        let html = first + "<span id=\"exampleand\"> and </span>" + second;
+        return html
     }
 }
 
 function select_example(): (string | [string, string]) {
-    return "a";
+    console.log(examples)
+    let example: string | [string, string] = examples[0]
+    return example;
 }
